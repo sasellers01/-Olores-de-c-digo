af7d3ae8e1ef6ea0ecc77ac8ff6b71e71ddf19c2
Sergio <sasellers01@educarex.es>
2026-01-28 19:59:16 +0100

    Refactor: Extracción de Métodos (importe y créditos)

--- fuentes/Facturador.java
+++ fuentes/Facturador.java
@@ -22,35 +22,44 @@ public class Facturador{
 
 		for(int i = 0; i < actuaciones.length; i++){
 			Integer iConcierto = actuaciones[i][0];
-			Double importeActuacion = 0d;
-
-			switch (repertorio[iConcierto][1]){
-				case "heavy":
-					importeActuacion = 4000d;
-					if (actuaciones[i][1] > 500)
-						importeActuacion += 20 * (actuaciones[i][1] - 500);
-					break;
-				case "rock":
-					importeActuacion = 3000d;
-					if (actuaciones[i][1] > 1000)
-						importeActuacion += 30 * (actuaciones[i][1] - 1000);
-					break;
-				default:
-					throw new Exception("Tipo de concierto desconocido.");
-			}
+			String tipo = repertorio[iConcierto][1];
+			Integer asistentes = actuaciones[i][1];
+			totalFactura += calcularImporteActuacion(tipo, asistentes);
+			creditos += calcularCreditos(tipo, asistentes);
+			System.out.println("\tConcierto: " + tipo);
+			System.out.println("\t\tAsistentes: " + asistentes);
 			
-			totalFactura += importeActuacion;
-
-			creditos += Math.max(actuaciones[i][1] - 500, 0);
-			if (repertorio[iConcierto][1].equals("heavy"))
-				creditos += actuaciones[i][1] / 5;
-
-			System.out.println("\tConcierto: " + repertorio[iConcierto][0]);
-			System.out.println("\t\tAsistentes: " + actuaciones[i][1]);
 		}
 		System.out.println("BASE IMPONIBLE: " + totalFactura + " euros");
 		System.out.printf("IVA (21%%): %.2f euros\n", totalFactura * 0.21);
 		System.out.printf("TOTAL FACTURA: %.2f euros\n", totalFactura * 1.21);
 		System.out.println("Créditos obtenidos: " + creditos);
+	}
+	
+	public static Double calcularImporteActuacion(String tipo, Integer asistentes) throws Exception {
+		Double importeActuacion = 0d;
+		switch (tipo){
+			case "heavy":
+				importeActuacion = 4000d;
+				if (asistentes > 500)
+					importeActuacion += 20 * (asistentes - 500);
+				break;
+			case "rock":
+				importeActuacion = 3000d;
+				if (asistentes > 1000)
+					importeActuacion += 30 * (asistentes - 1000);
+				break;
+			default:
+				throw new Exception("Tipo de concierto desconocido.");
+		}
+		return importeActuacion;
+	}
+	
+	public static Integer calcularCreditos(String tipo, Integer asistentes) throws Exception {
+		Integer creditos = 0;
+		creditos += Math.max(asistentes - 500, 0);
+		if (tipo.equals("heavy"))
+			creditos += asistentes / 5;
+		return creditos;
 	}
 }
